6113fc8c5a9e4a48b540b9ae80690392110fcf38
Breno Silva <brenophp@gmail.com>
2021-03-21 12:32:10 -0300

    adicionando config do git

---
+++ git_config.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --gloabl alias.ci commit
+git config --global alias.l log
+git config --global alias.lo log --oneline
+git config --global alias.d diff
+git config --global alias.a add

1800efdef602ea208ef6f3f13dd719e0f40f622f
Breno Silva <brenophp@gmail.com>
2021-03-25 12:10:25 -0300

    removendo config de git

--- git_config.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-git config --global alias.st status
-git config --global alias.br branch
-git config --global alias.co checkout
-git config --global alias.ci commit
-git config --global alias.l log
-git config --global alias.lo log --oneline
-git config --global alias.d diff
-git config --global alias.a add

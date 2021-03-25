cd66647e27e91e6082c1a357d2b97dc1d901902d
Breno Silva <brenophp@gmail.com>
2021-03-25 11:58:31 -0300

    corrigindo typo git config

--- git_config.sh
+++ git_config.sh
@@ -2,7 +2,7 @@
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.co checkout
-git config --gloabl alias.ci commit
+git config --global alias.ci commit
 git config --global alias.l log
 git config --global alias.lo log --oneline
 git config --global alias.d diff

83f1ee00e63a45a65ba25256719d3c80a7782015
Breno Silva <brenophp@gmail.com>
2021-03-25 12:09:31 -0300

    adicionando instalação do git

---
+++ install_git.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+sudo apt install git
+
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.l log
+git config --global alias.lo log --oneline
+git config --global alias.d diff
+git config --global alias.a add

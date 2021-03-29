9224a080a57d957c7299fe14e5ef37f443102dd6
Breno Silva <brenophp@gmail.com>
2021-03-29 14:20:20 -0300

    adding alias for checkout -p in git config

--- install_git.sh
+++ install_git.sh
@@ -6,6 +6,7 @@ git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.l log
-git config --global alias.lo log --oneline
+git config --global alias.lo "log --oneline"
 git config --global alias.d diff
 git config --global alias.a add
+git config --global alias.gap "checkout -p"

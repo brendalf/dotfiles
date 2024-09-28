20731abf58306bc79fcc85aff0ae2b03175f2abe
Breno Silva <brenophp@gmail.com>
2024-09-28 16:46:06 +0200

    feat: update setup.sh for macos

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,16 +1,10 @@
-echo "Updating Apt"
-sudo apt update
-
 echo "Installing Homebrew"
+cd
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/${USER}/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.config/fish/personal_config.fish
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo "Installing Dependencies"
-sudo apt install build-essential
-
-echo "Installing libraries"
-brew install gcc
+echo "Installing general packages"
 brew install git
 brew install gh
 brew install tmux tmuxinator
@@ -28,29 +22,20 @@ brew install ghq
 brew install exa
 brew install gnu-sed
 brew install nodejs
+brew install golang
 brew install rust
 brew install gum
+brew install zoxide
 
 echo "Github Auth"
 gh auth login
 
-echo "Clonning Dotfiles"
-cd /home/$USER
-gh repo clone brendalf/dotfiles
-mv dotfiles/.config/* .config/
-mv dotfiles/.git* .
-mv dotfiles/.tmux* .
-rm -r dotfiles
-
-echo "Instaling Packer"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 echo "Install and Configure Fish"
-sudo apt install fish
+brew install fish
 chsh -s $(which fish)
 
 echo "Install Fisher"
-curl -sL https://git.io/fisher > fisher.txt
+curl -sL https://git.io/fisher >fisher.txt
 fish -c "source fisher.txt"
 fish -c "fisher install jorgebucaran/fisher"
 rm fisher.txt
@@ -70,20 +55,6 @@ echo "Install Fisher Plugins"
 fish -c "fisher install gazorby/fish-abbreviation-tips"
 fish -c "fisher install jorgebucaran/nvm.fish"
 fish -c "fisher install IlanCosman/tide"
-fish -c "fisher install fisherman/pyenv"
-fish -c "fisher install jethrokuan/z"
-
-echo "Grant access to scripts"
-chmod +x .scripts/
-
-echo "Configure neovim"
-python3 -m pip install pynvim
-sudo update-alternatives --install /usr/bin/vi vi /home/linuxbrew/.linuxbrew/bin/nvim 60
-sudo update-alternatives --config vi
-sudo update-alternatives --install /usr/bin/vim vim /home/linuxbrew/.linuxbrew/bin/nvim 60
-sudo update-alternatives --config vim
-sudo update-alternatives --install /usr/bin/editor editor /home/linuxbrew/.linuxbrew/bin/nvim 60
-sudo update-alternatives --config editor
 
 echo "Configure Tmux"
 python3 -m pip install --user powerline-status
@@ -95,8 +66,3 @@ mkdir -p ~/.config/fontconfig/conf.d
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.fish
 mv tmuxinator.fish ~/.config/fish/completions/
-
-echo "Misc"
-npm install -g taskbook
-
-

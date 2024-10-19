089481e544cce928fb3f4117d894413c034bb605
Breno Silva <brenophp@gmail.com>
2024-10-19 15:07:41 +0200

    remove extra config from nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,16 +5,10 @@
 vim.g.python3_host_prog = "python"
 
 vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
 
 vim.opt.backup = false
 vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 
+vim.opt.hlsearch = true
 vim.opt.inccommand = "split"
-vim.opt.smarttab = true
 vim.opt.wrap = true

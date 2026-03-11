4c7942a39285a50d89cd2d73f8c4b4cf2cf9d7dd
Breno Silva <brenophp@gmail.com>
2026-03-12 00:35:55 +0200

    feat: telescope bindings outside config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,16 @@
 return {
   "nvim-telescope/telescope.nvim",
-  -- replace all Telescope keymaps with only one mapping
-  keys = function()
-    return {
-      { "<leader><Space>", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
-    }
-  end,
+  keys = {
+    { "<leader><Space>", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+    { "<Leader>fp", "<cmd>Telescope projects<CR>", desc = "Search for projects" },
+    { ";g", "<cmd>Telescope git_commits<CR>", desc = "Search git commits" },
+    { ";;", function() require("telescope.builtin").resume() end, desc = "Resume the previous telescope" },
+    { ";f", function() require("telescope.builtin").find_files({ no_ignore = true, hidden = true }) end, desc = "Find all files" },
+    { ";r", function() require("telescope.builtin").live_grep() end, desc = "Search for string in current directory" },
+    { ";e", function() require("telescope.builtin").diagnostics() end, desc = "List diagnostics" },
+    { ";b", function() require("telescope.builtin").buffers() end, desc = "List buffers" },
+    { ";s", function() require("telescope.builtin").treesitter() end, desc = "List function names and variables from treesitter" },
+  },
   config = function(_, opts)
     local telescope = require("telescope")
 
@@ -33,42 +38,5 @@ return {
 
     require("telescope").load_extension("git_worktree")
     require("telescope").load_extension("projects")
-
-    local find_all_files = function()
-      require("telescope.builtin").find_files({
-        no_ignore = true,
-        hidden = true,
-      })
-    end
-
-    local resume = function()
-      require("telescope.builtin").resume()
-    end
-
-    local live_grep = function()
-      require("telescope.builtin").live_grep()
-    end
-
-    local diagnostics = function()
-      require("telescope.builtin").diagnostics()
-    end
-
-    local buffers = function()
-      require("telescope.builtin").buffers()
-    end
-
-    local treesitter = function()
-      require("telescope.builtin").treesitter()
-    end
-
-    vim.keymap.set("n", "<Leader>fp", "<cmd>Telescope projects<CR>", { desc = "Seach for projects" })
-    vim.keymap.set("n", ";g", "<cmd>Telescope git_commits<CR>", { desc = "Seach for projects" })
-
-    vim.keymap.set("n", ";;", resume, { desc = "Resume the previous telescope" })
-    vim.keymap.set("n", ";f", find_all_files, { desc = "Find all files" })
-    vim.keymap.set("n", ";r", live_grep, { desc = "Search for string in current directory" })
-    vim.keymap.set("n", ";e", diagnostics, { desc = "List diagnostics" })
-    vim.keymap.set("n", ";b", buffers, { desc = "List buffers" })
-    vim.keymap.set("n", ";s", treesitter, { desc = "List function names and variables from treesitter" })
   end,
 }
